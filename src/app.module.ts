3adb3604177b342d43aac0a8ecd4545d566fa354
rrshidev <rrshidev@gmail.com>
2025-05-25 09:45:02 +0300

    config module configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,33 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { BotModule } from './bot/bot.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BotModule } from './bot/bot.module';
 import { UserModule } from './user/user.module';
 import { CarModule } from './car/car.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    BotModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'nika',
-      database: 'ServiceAutoDB',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
     }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+
+    BotModule,
     UserModule,
     CarModule,
   ],
