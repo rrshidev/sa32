20aefcbe5e27870f0a2b6c345195e11f12131e36
rrshidev <rrshidev@gmail.com>
2025-06-08 14:48:05 +0300

    full refactor of project!!

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,47 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { BotModule } from './bot/bot.module';
-import { UserModule } from './user/user.module';
-import { CarModule } from './car/car.module';
-import { CalendarModule } from './calendar/calendar.module';
-import { BullModule } from '@nestjs/bull';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
-
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-    }),
-
-    BotModule,
-    UserModule,
-    CarModule,
-    CalendarModule,
-    BullModule.forRoot({
-      redis: {
-        host: ConfigService.get<string>('REDIS_HOST'),
-        port: ConfigService.get<string>('REDIS_PORT'),
-      },
-    }),
-    BullModule.registerQueue({
-      name: 'notifications',
-    }),
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { BotService } from './bot/bot.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const botService = app.get(BotService);
-  await botService.startPolling();
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
